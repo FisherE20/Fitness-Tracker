f9bd527fd390d89ffe91047ad84177b6a554c8e4
Erin Fisher <flybirdie85@gmail.com>
2020-09-14 12:56:15 -0700

    added folder models and file workout

---
+++ models/workout.js
@@ -0,0 +1,61 @@
+// workout schema
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const WorkoutSchema = new Schema({
+    type: {
+        type: String,
+        trim: true
+    },
+
+    name: {
+        type: String,
+        trim: true
+    },
+
+    duration: {
+        type: Number,
+        trim: true
+    },
+
+    weight: {
+        type: Number,
+        trim: true
+    },
+
+    reps: {
+        type: Number,
+        trim: true
+    },
+
+    sets: {
+        type: Number,
+        trim: true
+    },
+
+    distance: {
+        type: Number,
+        trim: true
+    },
+
+    date: {
+        type: Date,
+        default: Date.now
+    },
+
+    excercise: []
+}, {
+    toJson: {
+        virtuals: true
+    }
+});
+
+WorkoutSchema.virtual("totalDuration").get(function () {
+    return this.exercise.reduce((total, exercise) => {
+        return total + exercise.duration;
+    }, 0);
+});
+
+const workout =  mongoose.model("Workout", WorkoutSchema);
+
+module.exports = workout;
