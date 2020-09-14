d06b0efd147a7cd2e6bbccb9fa517b0d11dd8f39
Erin Fisher <flybirdie85@gmail.com>
2020-09-14 15:47:19 -0700

    added index.js

--- server.js
+++ server.js
@@ -2,7 +2,8 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const logger = require("morgan");
-const path = require("./models");
+const path = require("path");
+const db = require("./models");
 
 
 const PORT = process.env.PORT || 3000;
@@ -16,7 +17,7 @@ app.use(express.json());
 app.use(express.static("public"));
 
 // Mongo connection
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
