68a1587afb3a675e508b4ea015ef9349cab949a6
Erin Fisher <flybirdie85@gmail.com>
2020-09-15 12:09:41 -0700

    Updated htmlRoutes to fix a bug

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,8 +1,8 @@
 const path = require("path")
 
 module.exports = function(app){
-    app.get("/excercise", function(req, res) {
-        res.sendFile(path.join(_dirname,"../public/excerise.html"))
+    app.get("/exercise", function(req, res) {
+        res.sendFile(path.join(_dirname,"../public/exercise.html"))
     })
 
     app.get("/stats",function(req, res) {
