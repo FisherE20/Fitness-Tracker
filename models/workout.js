9b920dbffdb421c5d1404e8e379c58a153f2e009
Erin Fisher <flybirdie85@gmail.com>
2020-09-16 14:59:35 -0700

    updated workout.js to debug

--- models/workout.js
+++ models/workout.js
@@ -3,52 +3,57 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const WorkoutSchema = new Schema({
-    type: {
-        type: String,
-        trim: true
-    },
-
-    name: {
-        type: String,
-        trim: true
-    },
-
-    duration: {
-        type: Number,
-        trim: true
-    },
-
-    weight: {
-        type: Number,
-        trim: true
-    },
-
-    reps: {
-        type: Number,
-        trim: true
-    },
-
-    sets: {
-        type: Number,
-        trim: true
-    },
-
-    distance: {
-        type: Number,
-        trim: true
-    },
-
     date: {
-        type: Date,
-        default: Date.now
-    },
+    type: Date,
+    default: Date.now
+},
 
-    excercise: []
+    exercises: [
+        {
+            type: {
+                type: String,
+                trim: true
+            },
+
+            name: {
+                type: String,
+                trim: true
+            },
+
+            duration: {
+                type: Number,
+                trim: true
+            },
+
+            weight: {
+                type: Number,
+                trim: true
+            },
+
+            reps: {
+                type: Number,
+                trim: true
+            },
+
+            sets: {
+                type: Number,
+                trim: true
+            },
+
+            distance: {
+                type: Number,
+                trim: true
+            }   
+        }
+    ]
+    
+    
 }, {
     toJson: {
         virtuals: true
     }
-});
+}
+);
 
 WorkoutSchema.virtual("totalDuration").get(function () {
     return this.exercise.reduce((total, exercise) => {
