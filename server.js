a8ff4054727d261cc96887e67a823645523c6323
Erin Fisher <flybirdie85@gmail.com>
2020-09-16 15:21:49 -0700

    deploying

--- server.js
+++ server.js
@@ -17,8 +17,10 @@ app.use(express.json());
 app.use(express.static("public"));
 
 // Mongo connection
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workouts", {
   useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
   useFindAndModify: false
 });
 
